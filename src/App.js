5c4306bf75e6abc77fc1e84be9f4b30bf7f779d2
chiko <ernestprabowo@gmail.com>
2022-10-11 02:07:17 +0700

    ver 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useEffect , useState} from "react";
+import MovieCard from './MovieCard'
+//96222cc5
 import './App.css';
+import SearchIcon from './search.svg';
+const API_URL = 'http://www.omdbapi.com?apikey=96222cc5';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App=()=>{
+  const [movies, setMovies] =  useState([]); 
+  const [searchTerm,setSearchTerm] = useState('');
+  const searchMovies = async(title) =>{// async function nya kan menerima title 
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+  useEffect(()=>{
+    searchMovies('spiderman');
+  }, []);
+  return(
+    // <h1>App</h1>
+    <div className="app">
+      <h1>NEZ Movies List</h1>
+      <div className="search">
+      <input type="text" placeholder="Search for movie" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
+      <img src={SearchIcon} alt="Search" onClick={()=>(searchMovies(searchTerm))}/>
+      </div>
+      {
+        movies?.length > 0 ?(
+            <div className="container">
+            {movies.map((movie)=>(
+              <MovieCard movie = {movie}/>
+            ))}
+            </div>
+          ):(
+            <div className="empty">
+              <h2>no movies found</h2>
+            </div>
+          )
+        };
     </div>
-  );
+    );
 }
-
 export default App;
+
